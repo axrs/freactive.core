10468c086a7124b5dfd1fbe0c38d6849ceb46613
Alexander Scott <xander@axrs.io>
2020-04-04 20:32:52 +1000

    Upgrade dependencies and fix symbol issue. Remove CLR

--- project.clj
+++ project.clj
@@ -1,15 +1,14 @@
-(defproject freactive.core "0.2.0-SNAPSHOT"
+(defproject io.axrs/freactive.core "0.2.0-SNAPSHOT"
   :description "Generic reactive atoms, expressions, cursors"
   :url "https://github.com/aaronc/freactive.core"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies
-  [[org.clojure/clojure "1.7.0-RC1"]
-   [org.clojure/data.avl "0.0.12"]]
+  [[org.clojure/clojure "1.10.1"]
+   [org.clojure/data.avl "0.1.0"]]
   :profiles
-  {:dev
-   {:dependencies 
-    [[org.clojure/clojurescript "0.0-3269"]]}}
-  :source-paths ["src/clojure"]
+  {:dev {:dependencies
+         [[org.clojure/clojurescript "1.10.597"]]}}
+  :source-paths ["src"]
   :javac-options ["-Xlint:unchecked"]
   :java-source-paths ["src/java"])
